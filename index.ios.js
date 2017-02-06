cbe222b07de00caf6347c288d50da9f0fa585930
hukai <532809319@qq.com>
2017-02-06 10:28:12 +0800

    it is image tag

--- index.ios.js
+++ index.ios.js
@@ -6,15 +6,15 @@
 
 import React, { Component } from 'react';
 import {
-  TouchableHighlight,
-  TabBarIOS,
+  // TouchableHighlight,
+  // TabBarIOS,
   AppRegistry,
   StyleSheet,
   Text,
   View,
   Image,
-  ActivityIndicatorIOS,
-  NavigatorIOS,
+  // ActivityIndicatorIOS,
+  // NavigatorIOS,
 } from 'react-native';
 class HeadText extends Component{
 
@@ -27,99 +27,65 @@ class HeadText extends Component{
   }
 }
 
-class Featured extends Component {
-  render (){
-    return (
-    //   <NavigatorIOS style={styles.container}
-    //   initialRoute={{
-    //     title:'推荐',
-    //  component:MovieTalk
-    //   }}
-    // />
-    <NavigatorIOS
-  style={
-    styles.container
-  }
-  initialRoute={{
-    title:'推荐电话',
-    component:HeadText
-  }}
-  shadowHidden='true'
-  barTintColor="darkslateblue"
-  titleTextColor="rgba(0,45,3,0.5)"
-    />
-    );
-  }
-}
+
 export default class MovieTalk extends Component {
-  constructor(props){
-    super(props);
-    this.state={
-      selectedTab:'user_box'
-    }
-  }
+
 
   render() {
     return (
 
-
-
-      <TabBarIOS barTintColor="darkslateblue" tintColor="white">
-        <TabBarIOS.Item systemIcon="featured"
-          selected={
-            this.state.selectedTab==='featured'
-          }
-          onPress={()=>{
-            this.setState({
-              selectedTab:'featured'
-            })
-          }}
-          >
-
-          <Text>
-                3啊啊啊啊ß
-          </Text>
-
-        </TabBarIOS.Item>
-        <TabBarIOS.Item systemIcon="most-viewed"
-          selected={this.state.selectedTab==='user_box'}
-           onPress={
-             ()=>{
-               this.setState({
-                 selectedTab:'user_box'
-               })
-             }
-           }>
-          <Text style={{marginTop:20}}>
-                4ds萨德
-          </Text>
-
-        </TabBarIOS.Item>
-
-        <TabBarIOS.Item systemIcon="most-viewed"
-          selected={this.state.selectedTab==='user_box'}
-           onPress={
-             ()=>{
-               this.setState({
-                 selectedTab:'user_box'
-               })
-             }
-           }>
-          <Featured />
-
-
-        </TabBarIOS.Item>
-
-      </TabBarIOS>
-
-
-
+  <View  style={styles.container}>
+    <Image
+style={styles.backgroundImage}
+source={{uri:'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1486344781&di=084a6ad23044ce78202f510a212b5bd5&src=http://c.hiphotos.baidu.com/image/pic/item/7af40ad162d9f2d38d7056c8acec8a136327ccb0.jpg'}}
+ //source={{uri:'http://a.hiphotos.baidu.com/image/pic/item/279759ee3d6d55fb924d52c869224f4a21a4dd50.jpg'}}
+  >
+  <View style={styles.overlay}>
+    <Text style={styles.overlayHeader}>在座的各位都是辣鸡</Text>
+    <Text style={styles.overlaySubHeader}>啊啊小标题</Text>
+  </View>
+  </Image>
+  </View>
 
     );
   }
 }
 
 const styles = StyleSheet.create({
+  overlay:{
+    backgroundColor: 'rgba(0,0,0,0.1)',
+    alignItems:'center'
+  },
+  overlayHeader:{
+    fontSize:33,
+    fontFamily:'Helvetica Neue',
+    fontWeight:'800',
+    color:'rgb(170, 102, 195)',
+    padding:10
+  },
+  overlaySubHeader:{
+    fontSize:16,
+    fontFamily:'Helvetica Neue',
+    fontWeight:'200',
+    padding:8,
+    paddingTop:0,
+    color:'rgb(13, 120, 189)'
+    //backgroundColor:'yellow'
+  },
+  backgroundImage:{
+    flex:1,
+    resizeMode:'cover',
+  },
+
+  image:{
+    width:100,
+    height:100,
+    // backgroundColor:'red',
+    margin:6,
+    resizeMode:'cover'
+
+  },
+
   item:{
     backgroundColor:'#fff',
     borderWidth:1,
@@ -144,7 +110,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     // flexDirection:'row',
-    backgroundColor:'#eaeff7',
+    // backgroundColor:'#eaeff7',
     paddingTop:23,
 
     // margin:30,
