1201bb52171df9d89358b81aecb12473f4214113
hukai <532809319@qq.com>
2017-02-07 16:03:26 +0800

    is for icon tabbar

--- index.ios.js
+++ index.ios.js
@@ -8,6 +8,7 @@ import React, { Component } from 'react';
 //import MovieList from './app/Components/MovieList';
 import ComeList from './app/Components/USBox';
 import MovieList from './app/Components/MovieList';
+import icons from './app/Assets/icon'
 import {
   // TouchableHighlight,
   // TabBarIOS,
@@ -33,9 +34,16 @@ export default class MovieTalk extends Component {
     }
   render(){
     return(
-      <TabBarIOS>
+      <TabBarIOS
+          barTintColor='#ccc'//底部底面颜色
+          tintColor='green'   //被选中颜色
+        >
           <TabBarIOS.Item
-              systemIcon="featured"
+              //systemIcon="featured"
+              icon={{uri:icons.star,scale:4}}
+              title="中国电影排行"
+
+              selectedIcon={{uri:icons.starActive , scale:5}}
               selected={this.state.selectedTab === 'featured'}
               onPress={()=>{
                 this.setState({
@@ -46,7 +54,10 @@ export default class MovieTalk extends Component {
             >
           <MovieList/>
           </TabBarIOS.Item>
-          <TabBarIOS.Item systemIcon="most-viewed"
+          <TabBarIOS.Item
+            //systemIcon="most-viewed"
+            icon={{uri:icons.movie,scale:3.5}}
+            title="北美票房"
                 selected={this.state.selectedTab === 'most'}
                 onPress={()=>{
                   this.setState({
